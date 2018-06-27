c6e56d771803094bb5fba2ff9831780a2cb6c738
rawmind0 <rawmind@gmail.com>
2018-06-27 15:27:59 +0200

    Added template to generate html

--- main.go
+++ main.go
@@ -2,69 +2,25 @@ package main
 
 import (
 	"fmt"
+	"github.com/rancher/hello-world/templates"
 	"net/http"
 	"os"
 	"regexp"
 	"strings"
 )
 
-const (
-	webHead = `<html>
-	<head>
-		<title>Rancher</title>
-		<link rel="icon" href="img/favicon.png">
-		<style>
-			body {
-				background-color: white;
-				text-align: center;
-				padding: 50px;
-				font-family: "Open Sans","Helvetica Neue",Helvetica,Arial,sans-serif;
-			}
-			button {
-			    background-color: #0075a8; 
-			    border: none;
-			    color: white;
-			    padding: 15px 32px;
-			    text-align: center;
-			    text-decoration: none;
-			    display: inline-block;
-			    font-size: 16px;
-			}
+type HelloWorldConfig struct {
+	Hostname string
+	Services map[string]string
+	Headers  http.Header
+	Host     string
+}
 
-			#logo {
-				margin-bottom: 40px;
-			}
-		</style>
-	</head>
-	<body>
-		<img id="logo" src="img/rancher-logo.svg" alt="Rancher logo" width=400 />
-		<h1>Hello world!</h1>`
+func (config *HelloWorldConfig) GetManifest() (string, error) {
+	return templates.CompileTemplateFromMap(templates.HelloWorldTemplate, config)
+}
 
-	webTail = `		<div class="row social">
-			<a class="p-a-xs" href="https://rancher.com/docs"><img src="img/favicon.png" alt="Docs" height="25" width="25"></a>
-			<a class="p-a-xs" href="https://slack.rancher.io/"><img src="img/icon-slack.svg" alt="slack" height="25" width="25"></a>
-			<a class="p-a-xs" href="https://github.com/rancher/rancher"><img src="img/icon-github.svg" alt="github" height="25" width="25"></a>
-			<a class="p-a-xs" href="https://twitter.com/Rancher_Labs"><img src="img/icon-twitter.svg" alt="twitter" height="25" width="25"></a>
-			<a class="p-a-xs" href="https://www.facebook.com/rancherlabs/"><img src="img/icon-facebook.svg" alt="facebook" height="25" width="25"></a>
-			<a class="p-a-xs" href="https://www.linkedin.com/groups/6977008/profile"><img src="img/icon-linkedin.svg" height="25" alt="linkedin" width="25"></a>
-        </div>	
-		<script>
-			function myFunction() {
-			    var x = document.getElementById("` + reqInfoID + `");
-			    if (x.style.display === "none") {
-			        x.style.display = "block";
-			    } else {
-			        x.style.display = "none";
-			    }
-			}
-		</script>
-	</body>
-</html>`
-
-	reqInfoID = "reqInfo"
-)
-
-func getServices() map[string]string {
+func (config *HelloWorldConfig) getServices() {
 	k8sServices := make(map[string]string)
 
 	for _, evar := range os.Environ() {
@@ -76,36 +32,25 @@ func getServices() map[string]string {
 		}
 	}
 
-	return k8sServices
+	config.Services = k8sServices
+}
+
+func (config *HelloWorldConfig) Init(r *http.Request) {
+	config.Hostname, _ = os.Hostname()
+	config.Host = r.Host
+	config.Headers = r.Header
+	config.getServices()
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	hostname, _ := os.Hostname()
-	k8sServices := getServices()
-
-	fmt.Fprintln(w, webHead)
-	fmt.Fprintln(w, "		<h3>My hostname is ", hostname, "</h3>")
-	k8sNumber := len(k8sServices)
-	if k8sNumber > 0 {
-		fmt.Fprintln(w, "		<h3>k8s services found,", k8sNumber, "</h3>")
-		for k, v := range k8sServices {
-			fmt.Fprintln(w, "		<b>", k, "</b> ", v, "<br />")
-		}
+	config := &HelloWorldConfig{}
+	config.Init(r)
+	data, err := config.GetManifest()
+	if err != nil {
+		fmt.Fprintln(w, err)
 	}
-	fmt.Fprintln(w, "		<br />")
 
-	fmt.Fprintln(w, "		<button class='button' onclick='myFunction()'>Show details</button>")
-	fmt.Fprintln(w, "		<div id='"+reqInfoID+"' style='display:none'>")
-	fmt.Fprintln(w, "			<h3>Request info</h3>")
-	fmt.Fprintln(w, "			<b>Host:</b> ", r.Host, "<br />")
-	fmt.Fprintln(w, "			<b>Pod:</b> ", hostname, "</b><br />")
-	for k, v := range r.Header {
-		if strings.HasPrefix(k, "X-") {
-			fmt.Fprintln(w, "		<b>", k, "</b> ", v, "<br />")
-		}
-	}
-	fmt.Fprintln(w, "		<br /></div>")
-	fmt.Fprintln(w, webTail)
+	fmt.Fprint(w, data)
 }
 
 func main() {
---
+++ templates/hello-world.go
@@ -0,0 +1,88 @@
+package templates
+
+const (
+	reqInfoID = "reqInfo"
+
+	webHead = `<html>
+  <head>
+    <title>Rancher</title>
+    <link rel="icon" href="img/favicon.png">
+    <style>
+      body {
+        background-color: white;
+        text-align: center;
+        padding: 50px;
+        font-family: "Open Sans","Helvetica Neue",Helvetica,Arial,sans-serif;
+      }
+      button {
+          background-color: #0075a8; 
+          border: none;
+          color: white;
+          padding: 15px 32px;
+          text-align: center;
+          text-decoration: none;
+          display: inline-block;
+          font-size: 16px;
+      }
+
+      #logo {
+        margin-bottom: 40px;
+      }
+    </style>
+  </head>
+  <body>
+    <img id="logo" src="img/rancher-logo.svg" alt="Rancher logo" width=400 />
+    <h1>Hello world!</h1>
+    <h3>My hostname is {{.Hostname}}</h3>`
+
+	webServices = `{{- $length := len .Services }} 
+  {{- if gt $length 0 }}
+    <div id='Services'>
+      <h3>k8s services found {{$length}}</h3>
+    {{ range $k,$v := .Services }}
+      <b>{{ $k }}</b>{{ $v }}<br />
+    {{ end }}
+    </div>
+    <br />
+  {{ end }}`
+
+	webDetails = `    <button class='button' onclick='myFunction()'>Show request details</button>
+    <div id='reqInfo' style='display:none'>
+      <h3>Request info</h3>
+      <b>Host:</b> {{.Host}} <br />
+      <b>Pod:</b> {{.Hostname}} </b><br />
+    {{ range $k,$v := .Headers }}
+      <b>{{ $k }}</b>{{ $v }}<br />
+    {{ end }}
+    </div>
+    <br />`
+
+	webLinks = `    <div id='rancherLinks' class="row social">
+      <a class="p-a-xs" href="https://rancher.com/docs"><img src="img/favicon.png" alt="Docs" height="25" width="25"></a>
+      <a class="p-a-xs" href="https://slack.rancher.io/"><img src="img/icon-slack.svg" alt="slack" height="25" width="25"></a>
+      <a class="p-a-xs" href="https://github.com/rancher/rancher"><img src="img/icon-github.svg" alt="github" height="25" width="25"></a>
+      <a class="p-a-xs" href="https://twitter.com/Rancher_Labs"><img src="img/icon-twitter.svg" alt="twitter" height="25" width="25"></a>
+      <a class="p-a-xs" href="https://www.facebook.com/rancherlabs/"><img src="img/icon-facebook.svg" alt="facebook" height="25" width="25"></a>
+      <a class="p-a-xs" href="https://www.linkedin.com/groups/6977008/profile"><img src="img/icon-linkedin.svg" height="25" alt="linkedin" width="25"></a>
+    </div>
+    <br />`
+
+	webTail = `    <script>
+      function myFunction() {
+          var x = document.getElementById("` + reqInfoID + `");
+          if (x.style.display === "none") {
+              x.style.display = "block";
+          } else {
+              x.style.display = "none";
+          }
+      }
+    </script>
+  </body>
+</html>`
+
+	HelloWorldTemplate = webHead + `
+` + webServices + `
+` + webLinks + `
+` + webDetails + `
+` + webTail
+)
